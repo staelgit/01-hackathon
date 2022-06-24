2a7ccc766d38fcc2cf00c42f7747315dddda199c
styura3000 <styura3000@gmail.com>
2022-06-24 20:10:30 +0300

    create menu whith example module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
-import './styles.css'
+import './styles.css';
 
+import { ContextMenu } from './menu';
+
+import { BackgroundModule } from './modules/background.module';
+import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
+
+import { SayKuku } from './modules/kuku.module'; // для примера
+
+const contextMenu = new ContextMenu('.menu');
+
+contextMenu.add(new SayKuku());                 // для примера
+// contextMenu.add(new BackgroundModule());
+// contextMenu.add(new ClicksModule());
+// contextMenu.add(new ShapeModule());
+
+document.body.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+  contextMenu.open();
+  contextMenu.el.style.top = `${event.clientY}px`;
+  contextMenu.el.style.left = `${event.clientX}px`;
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,25 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+  }
+  open() {
+    this.el.classList.add('open');
+  }
 
+  close() {
+    this.el.classList.remove('open');
+  }
+
+  add(menuItem) {
+    const menuItemString = menuItem.toHTML();
+    this.el.insertAdjacentHTML('beforeend', menuItemString);
+    const menuItemHTML = this.el.querySelector(`[data-type="${menuItem.type}"]`);
+    menuItemHTML.addEventListener('click', () => {
+      menuItem.trigger();
+      this.el.classList.remove('open');
+    })
+    
+  }
 }
