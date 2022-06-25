b60d9ce6794b7bce3b3e40bb0453ae39c85840ed
artyk <artikocon@gmail.com>
2022-06-25 10:52:26 +0300

    Я закончил с фоном

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,26 +2,14 @@ import { Module } from "../core/module";
 import { random } from "../utils";
 
 export class BackgroundModule extends Module {
-  constructor(type, text) {
-    super(type, text);
-  }
-  toHTML() {
-    const uList = document.querySelector(".menu");
-    const newElement = uList.insertAdjacentHTML(
-      "afterend",
-      `<li id="color" class="menu-item" data-type="${this.type}">${this.text}</li>`
-    );
-    return newElement;
+  constructor() {
+    super("background", "Change the background color");
   }
   setColor() {
     return random(100, 255);
   }
-
   trigger() {
-    const btn = document.querySelector("#color");
-    btn.addEventListener("click", () => {
-      document.body.style.backgroundColor =
-        "rgb(" + setColor() + "," + setColor() + "," + setColor() + ")";
-    });
+    document.body.style.backgroundColor =
+      "rgb(" + setColor() + "," + setColor() + "," + setColor() + ")";
   }
 }
